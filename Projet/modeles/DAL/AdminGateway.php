867bdd92c92e04e84f5f9b7a9059b5cad5eae460
Noan07 <84435602+Noan07@users.noreply.github.com>
2022-12-22 21:09:03 +0100

    Update AdminGateway

--- Projet/modeles/DAL/AdminGateway.php
+++ Projet/modeles/DAL/AdminGateway.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once "config/Connection.php";
+    class AdminGateway {
+        protected Connection $con;
 
-class AdminGateway {
-    protected Connection $con;
+        public function __construct(Connection $con)
+        {
+            $this->con=$con;
+        }
 
-    public function __construct(Connection $con)
-    {
-        $this->con=$con;
+
+        //pas utiliser pour le moment, le but est qu'un administrateur puisse faire d'un utilisateur un administrateur
+        public function ajoutUtilisateur($nom,$email,$mdp): void{
+            $query = "INSERT INTO `utilisateur` (`nom`, `mail`, `mdp`,`role`) VALUES('$nom', '$email', '$mdp','Admin')";
+            $this->con->executeQuery($query);
+        }
     }
-
-    public function ajoutUtilisateur($nom,$email,$mdp): void{
-        $query = "INSERT INTO `utilisateur` (`nom`, `mail`, `mdp`,`role`) VALUES('$nom', '$email', '$mdp','Admin')";
-        $this->con->executeQuery($query);
-    }
-}
 ?>
